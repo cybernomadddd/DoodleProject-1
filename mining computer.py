8cbaea16b3b290d6c1b9c77ccf6b580e28681b5c
CyberWarny <154029339+cybernomadddd@users.noreply.github.com>
2024-03-08 17:51:12 -0500

    added python files
    the main file i've worked on for this project.

---
+++ mining computer.py
@@ -0,0 +1,53 @@
+import time
+import random
+while True:
+        reply = input("Drill Parameters[Drill(a), Depth(b), Yaw(c), Roll(d), Pitch(e), Ore density(f), Temperature(g),  ̶D̶E̶E̶P̶D̶A̶R̶K̶C̶A̶V̶E([REDACTED])̶, Help(h):]")
+        if reply == 'a':
+            print("running drill diagnostics")
+            time.sleep(2)
+            print("...")
+            time.sleep(2)
+            print("...")
+            time.sleep(2)
+            print("...")
+            time.sleep(2)
+            print("...")
+            time.sleep(2)
+            print("Drill bit strength approx. 78%")
+
+        if reply == 'b':
+            print("Drill approx. depth: ")
+
+        if reply == 'c':
+            print("3.14 degrees approx. reit at constant:1 manual setting")
+
+        if reply == 'd':
+            print("5.24 degrees approx. reit at constant: 3 auxiliary setting")
+
+        if reply == 'e':
+            print(".12 degrees approx. reit at constant: 0.03 stablized setting")
+
+        if reply == 'f':
+            print("Our CentroWarnian Standard Ore analyzers have identified the surrounding ore fields. Approx density: 5/1pSqM[Read as '5 to 1 per square meter'].")
+
+        if reply == 'g':
+            print("Out.temp: 80f [Stable]")
+
+        if reply == 'h':
+
+            replyheirc2 = input("Help Menu[YPR settings(a), Emergency manual(b), Analysis guide(c), eNcOUNtEr ScENARiO(xyz)]")
+
+            if replyheirc2 == 'a':
+                print("The Yaw, Pitch, and Roll of the drill pod can be set onto 3 default settings: Auxilary, Manual, or Stablized. Auxiliary setting allows for constant changes in YPR to allow for greater performance(Useful for hard ore drilling). Manual allows for the pilot to have full control with more versitility(Good for tunnel boring). Stablized allows for machine aided stability(Helpful in single-axis drilling).") 
+            if replyheirc2 == 'b':
+                print("In all major emergency scenarios, the radio flare cache in the lower computing core should be activated. Oxygen reserves and PSI power generators are availble within the emergency clot. Emergency rations(2 weeks) are found in the back of the primary computing core.")
+            if replyheirc2 == 'c':
+                print("To analyze surrounding ores, connect the ore radar to a power hole. Turn on the ore radar and and choose the scan radius[the larger the radius, the less accurate]. A popup will appear with 3 options. Option 1 allows you to view the ore types and values. Option 2 allows you to view the predicted surrounding ores and Option 3 allows for ground composition and analysis.")
+            if replyheirc2 == 'xyz':
+                print("")
+            if replyheirc2 == 's':
+                print("")
+            
+
+
+
